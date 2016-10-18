f63bb4e4e006bdcd3fd2da40cdc67a4e5ff2d9cf
unicodeveloper <prosperotemuyiwa@gmail.com>
2016-10-18 23:31:41 +0100

    Fix: Remove middleware preventing videos from loading on the homepage

--- server/routes.js
+++ server/routes.js
@@ -16,7 +16,7 @@ module.exports = function(app) {
 
   app.post('/api/videos/create', token.ensureAuthenticated, Video.create);
   app.put('/api/video/:public_id', token.ensureAuthenticated, Video.updateVideoDetails);
-  app.get('/api/videos', token.ensureAuthenticated, Video.retrieveAll);
+  app.get('/api/videos', Video.retrieveAll);
   app.get('/api/video/:public_id', token.ensureAuthenticated, Video.retrieveEachVideoDetails);
   app.delete('/api/video/:public_id', token.ensureAuthenticated, Video.deleteVideo);
 
